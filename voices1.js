8df1697dea7e794096ac334ba9e2eb64b428d232
GoodDeveloper <100660950+Good-Web-Dev@users.noreply.github.com>
2023-01-22 21:47:25 +0300

    Add files via upload

---
+++ voices1.js
@@ -0,0 +1,141 @@
+console.log("After Reading Go Here!")
+
+var ar1 = new Audio("https://files.soundoftext.com/9abb08e0-90e6-11ed-a44a-8501b7b1aefa.mp3");
+
+var ar2 = new Audio("https://files.soundoftext.com/b373ec30-90e6-11ed-a44a-8501b7b1aefa.mp3");
+
+var ar3 = new Audio("https://files.soundoftext.com/bb918f30-90e6-11ed-a44a-8501b7b1aefa.mp3");
+
+var ar4 = new Audio("https://files.soundoftext.com/c1091870-90e6-11ed-a44a-8501b7b1aefa.mp3");
+
+var ar5 = new Audio("https://files.soundoftext.com/c4fbd670-90e6-11ed-a44a-8501b7b1aefa.mp3");
+
+var ar6 = new Audio("https://files.soundoftext.com/c9d94290-90e6-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Nouns Go Here!");
+
+var n1 = new Audio("https://files.soundoftext.com/b475f4f0-641a-11ed-a44a-8501b7b1aefa.mp3");
+
+var n2 = new Audio("https://files.soundoftext.com/5a50fe80-4b14-11ed-a44a-8501b7b1aefa.mp3");
+
+var n3 = new Audio("https://files.soundoftext.com/b2720540-57fe-11ed-a44a-8501b7b1aefa.mp3");
+
+var n4 = new Audio("https://files.soundoftext.com/5c0c7900-4715-11ed-a4bb-5deaa2337781.mp3");
+
+var n5 = new Audio("https://files.soundoftext.com/df5f8980-4d0c-11ed-a44a-8501b7b1aefa.mp3");
+
+var n6 = new Audio("https://files.soundoftext.com/175e0a10-55f9-11ed-a44a-8501b7b1aefa.mp3");
+
+var n7 = new Audio("https://files.soundoftext.com/f8466270-90eb-11ed-a44a-8501b7b1aefa.mp3");
+
+var n8 = new Audio('https://files.soundoftext.com/6bef1aa0-5db1-11ed-a44a-8501b7b1aefa.mp3');
+
+			var n9 = new Audio('https://files.soundoftext.com/daa83ac0-4b92-11ed-a44a-8501b7b1aefa.mp3');
+			
+			var n10 = new Audio("https://files.soundoftext.com/a06ac190-5f40-11ed-a44a-8501b7b1aefa.mp3");
+			
+			var n11 = new Audio("https://files.soundoftext.com/2eb00480-6359-11ed-a44a-8501b7b1aefa.mp3");
+			
+console.log("Verbs Go Here!");
+
+var v1 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=abjust&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v2 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=avoid%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v3 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Crash&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v4 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=encounter&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v5 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=misjudge&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v6 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Obey&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v7 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=occur&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v8 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=pay%20attention%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v9 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=prevent&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v10 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=release&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v11 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=skid&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v12 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=speed&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v13 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=swerve&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v14 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=tailgate&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var v15 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=weave&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+console.log("Verbs For Injuries Go Here!");
+
+var vfi1 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=break&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var vfi2 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=burn&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var vfi3 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=cut&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var vfi4 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=die&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var vfi5 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=fracture&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var vfi6 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=poke&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var vfi7 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=scar&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var vfi8 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=slip&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var vfi9 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=sprain&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+console.log("Adjectives Go Here!");
+
+var adj1 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Aggressive%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var adj2 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=cautious%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var adj3 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=distracted%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var adj4 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=drowsy&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var adj5 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=injured&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var adj6 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=rude&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var adj7 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=severe&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var adj8 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=unconscious%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+console.log("Reflexive-Pronouns Go Here!")
+
+var rp1 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=herself&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rp2 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=himself&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rp3 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=itself&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rp4 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Myself&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rp5 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=ourselves%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rp6 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Themselves%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rp7 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=yourself&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rp8 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=yourselves%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+console.log("Conjunctions Go Here!");
+
+var con1 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Because%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var con2 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=So&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+console.log("Real Talk Go Here");
+
+var rt1 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=I%20guess%20.%20Expression%20to%20show%20that%20you%E2%80%99re%20not%20totally%20sure%20about%20a%20statement.&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rt2 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Lucky%20day%20.%20A%20day%20of%20good%20fortune.&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rt3 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Stuck%20in%20.%20Caught%20and%20unable%20to%20move%20out.&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rt4 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Look%20at%20it%20this%20way%20.%20There%E2%80%99s%20a%20different%20way%20to%20think%20about%20something.&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
---
+++ voices2.js
@@ -0,0 +1,91 @@
+console.log("Nouns Go Here!");
+
+var n1 = new Audio("https://files.soundoftext.com/7b8499d0-6019-11ed-a44a-8501b7b1aefa.mp3");
+
+var n2 = new Audio("https://files.soundoftext.com/2cf51e40-7656-11ed-a44a-8501b7b1aefa.mp3");
+
+var n3 = new Audio("https://files.soundoftext.com/9df13340-8de9-11ed-a44a-8501b7b1aefa.mp3");
+
+var n4 = new Audio("https://files.soundoftext.com/a178d360-8de9-11ed-a44a-8501b7b1aefa.mp3");
+
+var n5 = new Audio("https://files.soundoftext.com/6be5c200-6f5b-11ed-a44a-8501b7b1aefa.mp3");
+
+var n6 = new Audio("https://files.soundoftext.com/af543060-8de9-11ed-a44a-8501b7b1aefa.mp3");
+
+var n7 = new Audio("https://files.soundoftext.com/b3c5bb50-8de9-11ed-a44a-8501b7b1aefa.mp3");
+
+var n8 = new Audio("https://files.soundoftext.com/ef2efca0-7701-11ed-a44a-8501b7b1aefa.mp3");
+
+var n9 = new Audio("https://files.soundoftext.com/bfde0b90-8de9-11ed-a44a-8501b7b1aefa.mp3");
+
+var n10 = new Audio("https://files.soundoftext.com/c75423f0-8de9-11ed-a44a-8501b7b1aefa.mp3");
+
+var n11 = new Audio("https://files.soundoftext.com/a6177530-5e3b-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Verbs Go Here!");
+
+var v1 = new Audio("https://files.soundoftext.com/850a58b0-51bd-11ed-a44a-8501b7b1aefa.mp3");
+
+var v2 = new Audio("https://files.soundoftext.com/6e0adcb0-8deb-11ed-a44a-8501b7b1aefa.mp3");
+
+var v3 = new Audio("https://files.soundoftext.com/1939c700-5ce0-11ed-a44a-8501b7b1aefa.mp3");
+
+var v4 = new Audio("https://files.soundoftext.com/a570fd60-6f91-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Verbs And Expressions For Advice Go Here!");
+
+var vea1 = new Audio("https://files.soundoftext.com/e13ec470-4ad5-11ed-a44a-8501b7b1aefa.mp3");
+
+var vea2 = new Audio("https://files.soundoftext.com/cf121360-8dec-11ed-a44a-8501b7b1aefa.mp3");
+
+var vea3 = new Audio("https://files.soundoftext.com/d07c6570-64f2-11ed-a44a-8501b7b1aefa.mp3");
+
+var vea4 = new Audio("https://files.soundoftext.com/c04db5f0-8dec-11ed-a44a-8501b7b1aefa.mp3");
+
+var vea5 = new Audio("https://files.soundoftext.com/ae4dffb0-48f8-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Two-Word Verb Goes Here!");
+
+var $2wv1 = new Audio("https://files.soundoftext.com/5d53d940-4b4b-11ed-a44a-8501b7b1aefa.mp3");
+
+var $2wv2 = new Audio("https://files.soundoftext.com/dd5b0940-7785-11ed-a44a-8501b7b1aefa.mp3");
+
+var $2wv3 = new Audio("https://files.soundoftext.com/f74679b0-8ded-11ed-a44a-8501b7b1aefa.mp3");
+
+var $2wv4 = new Audio("https://files.soundoftext.com/a30d3780-6f07-11ed-a44a-8501b7b1aefa.mp3");
+
+var $2wv5 = new Audio("https://files.soundoftext.com/dbd10f30-4a25-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Three-Word Verb Goes Here!")
+
+var $3wv1 = new Audio("https://files.soundoftext.com/6d4b9210-8df0-11ed-a44a-8501b7b1aefa.mp3");
+
+var $3wv2 = new Audio("https://files.soundoftext.com/7d0cc610-8df0-11ed-a44a-8501b7b1aefa.mp3");
+
+var $3wv3 = new Audio("https://files.soundoftext.com/1b174660-6fb4-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Adjectives Go Here!");
+
+var adj1 = new Audio("https://files.soundoftext.com/77ce8b70-8f94-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj2 = new Audio("https://files.soundoftext.com/9d63a420-67e8-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj3 = new Audio("https://files.soundoftext.com/816fe840-8f94-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj4 = new Audio("https://files.soundoftext.com/33ac6d10-4dde-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj5 = new Audio("https://files.soundoftext.com/6109be50-4ae8-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj6 = new Audio("https://files.soundoftext.com/d78445c0-49e2-11ed-a44a-8501b7b1aefa.mp3");
+
+var adj7 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Old-fashioned%20&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var adj8 = new Audio("https://files.soundoftext.com/2a1503a0-66fe-11ed-a44a-8501b7b1aefa.mp3");
+
+console.log("Real Talk Go Here!");
+
+var rt1 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Feeling%20down&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rt2 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=Look%20out%20for%20myself&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
+
+var rt3 = new Audio("https://translate.google.com/translate_tts?ie=UTF-8&q=To%20be%20honest&tl=en-US&total=1&idx=0&client=tw-ob&ttsspeed=1");
